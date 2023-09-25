028d4bca4a045b6ffe092637b1c46d1a42a0631f
geraldy.miero <geraldy.miero@maucash.id>
2023-09-26 00:03:59 +0700

    update: add validator

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const validator = require('express-validator')
 
 const router = express.Router()
 
@@ -6,9 +7,20 @@ router.get('/', (req, res) => {
     res.render('form', { title: 'Regis form' })
 })
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    res.render('form', { title: 'Regis form' })
+router.post('/', 
+[
+    validator.body('name').isLength({ min: 1 }).withMessage('Please enter a name')
+    // body('name')
+    //     .isLength({ min: 1 })
+    //     .withMessage('Please enter a name')
+],
+(req, res) => {
+    const err = validator.validationResult(req)
+    if (err.isEmpty()) {
+        res.send('Welcome')
+    } else {
+        res.render('form', { title: 'Regis form', errors: err.array(), data: req.body })
+    }
 })
 
 module.exports = router
